0ccff9235d089ae70d9b30d498b60c0f2485a1f8
FNazare <filipenazare0@gmail.com>
2021-04-08 23:38:24 +0100

    created models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,37 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+#POST
+class Post(models.Model):
+    content = models.CharField(max_length=255)
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return f"{self.content}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "poster": self.user,
+            "content": self.content,
+            "likes": self.likes.all().count()
+        }
+
+#LIKE
+class Like(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='likes_given')
+    like = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
+
+#FOLLOW
+class Follow(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='followers')
+    follower = models.ManyToManyField("User", related_name='follows')
+
+    def __str__(self):
+        return f"{self.follower.all()} follows {self.user}"
