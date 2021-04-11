b9528e50b98f611c3c33d87b0879f265d8f64e8d
FNazare <filipenazare0@gmail.com>
2021-04-11 17:48:58 +0100

    modify Post model to include timestamp field

--- network/models.py
+++ network/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
 class Post(models.Model):
     content = models.CharField(max_length=255)
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='posts')
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.content}"
@@ -19,6 +20,7 @@ class Post(models.Model):
             "id": self.id,
             "poster": self.user.username,
             "content": self.content,
+            "timestamp": self.timestamp.ctime(),
             "likes": self.likes.all().count()
         }
 
